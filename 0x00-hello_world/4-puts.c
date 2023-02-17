d5ca3f2c7a2f14b671c4d7b2804a82fdb4bea34b
Raphchuzy <dr.rapchuzy@gmail.com>
2023-02-17 10:55:20 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A Program that prints a line using thr Put Fumction
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

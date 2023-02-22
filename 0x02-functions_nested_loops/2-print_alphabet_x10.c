5cbe8586c58caead14099b8f7f6c51590172b65f
Raphchuzy <dr.rapchuzy@gmail.com>
2023-02-22 15:35:44 -0800

    2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
 /**
  * print_alphabet_x10 - Make alphabet x10 times
  * Return: void
  */
-
 void print_alphabet_x10(void)
 {
 	char c;
@@ -16,8 +14,7 @@ void print_alphabet_x10(void)
 		{
 			_putchar(c);
 		}
-		_putchar(c);
+		_putchar('\n');
 		i++;
 	}
 }
-
